fd47b15e230e0ec44f977788b7a0997cd76f1bd7
safi41 <jadoonsufiankhan@gmail.com>
2025-05-29 12:30:19 +0500

    Update web build with latest changes

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"cf56914b326edb0ccb123ffdc60f00060bd513
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "887477986"
+    serviceWorkerVersion: "3112816949"
   }
 });
